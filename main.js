db88994f1ebbe9a2c2d2cea44d18c59c35cfec73
Dylan McKee <dylan@djmckee.co.uk>
2015-02-24 19:13:35 +0000

    Merged hideyshow.js into main.js
    Merged hideyshow.js into main.js

--- main.js
+++ main.js
@@ -378,4 +378,22 @@ $(document).ready(function(){
     deleteAnnotationAtIndex(index);
   });
 
+  //hidey-show
+  $("#showVA").hide();
+
+  $("#hideVA").click(function(){
+    $("#hideVA").hide();
+    $("#showVA").show();
+    $('video').animate({"width" : "600px"});
+    $('#vidAnnotation').hide();
+  });
+
+  $("#showVA").click(function(){
+    $("#showVA").hide();
+    $("#hideVA").show();
+    $('video').animate({"width" : "400px"}, function(){
+      $("#vidAnnotation").show();
+    });
+  });
+
 });
