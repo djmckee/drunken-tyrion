15ec751fae02fb4a7bdcf6359a3b6241592daaf9
Philip Beck <ps.beck@gmail.com>
2015-03-02 19:02:29 +0000

    link display improvements

--- main.js
+++ main.js
@@ -701,7 +701,7 @@ function populateAnnotationsList() {
             else {
                 linkFormatted = currentAnnotation.link;
             }
-            linkText = '<div class="annotationLink" title="' + currentAnnotation.link + '"><a href="' + currentAnnotation.link + '" target="_blank">' + linkFormatted + '</a></div>';
+            linkText = '<div class="annotationLink" title="' + currentAnnotation.link + '"><a href="' + currentAnnotation.link + '" target="_blank"><i class="fa fa-link"></i> ' + linkFormatted + '</a></div>';
         }
 
         console.log(linkText);
