059a9f795f91fa2b2bb8f0d5572970f3fa76d4de
Dylan McKee <dylan@djmckee.co.uk>
2015-03-02 12:43:48 +0000

    Image annotations now show in the list view too.

--- main.js
+++ main.js
@@ -639,13 +639,17 @@ function populateAnnotationsList() {
         var annotationTypeString = 'Text annotation';
 
         //check to see if it's a fancy image one?
+        var imageElement = '';
         if (currentAnnotation.imageUrl != null){
           //okay there's an image url... it's actually an image annotation...
           annotationTypeString = 'Image annotation';
+
+          //create an image tag...
+          imageElement = '<img style="width: 100%; height: auto; margin-top: 5px;" src="' + currentAnnotation.imageUrl + '"/>';
         }
 
         //formulate our new li HTML...
-        var newListElement = '<li class="vidAnnotationListItem" style="background-color: ' + currentAnnotation.backgroundColour + ';" ><a class="removeAnnotation" href="#" data-easyannotation-annotation-id="' + i + '">X</a><div class="vidAnnotationType">' + annotationTypeString + '</div><div class="vidAnnotationTimes">' + formatSecondsToString(currentAnnotation.startTime) + ' - ' + formatSecondsToString(currentAnnotation.endTime) + '</div><div class="vidAnnotationContent" style="color: '  + currentAnnotation.textColour + ';">' + currentAnnotation.textString + '</div></li>';
+        var newListElement = '<li class="vidAnnotationListItem" style="background-color: ' + currentAnnotation.backgroundColour + ';" ><a class="removeAnnotation" href="#" data-easyannotation-annotation-id="' + i + '">X</a><div class="vidAnnotationType">' + annotationTypeString + '</div><div class="vidAnnotationTimes">' + formatSecondsToString(currentAnnotation.startTime) + ' - ' + formatSecondsToString(currentAnnotation.endTime) + '</div><div class="vidAnnotationContent" style="color: '  + currentAnnotation.textColour + ';">' + imageElement + currentAnnotation.textString + '</div></li>';
         //and add it to the end of the list...
         $("ul#vidAnnotationList").append(newListElement);
     }
